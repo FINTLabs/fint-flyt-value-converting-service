33831f55c8200ee1ce19075bd02c28961c5f66e4
Egil Ballestad <egil.ballestad@vigoiks.no>
2024-08-29 08:42:21 +0200

    add unittests for userpermissions in controller

--- src/test/java/no/fintlabs/ValueConvertingControllerTest.java
+++ src/test/java/no/fintlabs/ValueConvertingControllerTest.java
@@ -9,14 +9,17 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -55,7 +58,7 @@ public class ValueConvertingControllerTest {
 
     @Test
     @SuppressWarnings("unchecked")
-    public void testGetValueConvertings() {
+    public void shouldReturnAllValueConvertingsWithUserPermissionsDisabled() {
         Page<ValueConvertingDto> mockPage = mock(Page.class);
         when(valueConvertingService.findAll(any(PageRequest.class), anyBoolean())).thenReturn(mockPage);
 
@@ -66,7 +69,32 @@ public class ValueConvertingControllerTest {
     }
 
     @Test
-    public void testGetValueConvertingFound() {
+    @SuppressWarnings("unchecked")
+    public void shouldReturnSpecificValueConvertingsWithUserPermissionsEnabled() throws NoSuchFieldException, IllegalAccessException {
+        setUserPermissionsConsumerEnabled();
+
+        Jwt jwt = mock(Jwt.class);
+        when(jwt.getClaimAsString("sourceApplicationIds")).thenReturn("1,2");
+
+        when(authentication.getPrincipal()).thenReturn(jwt);
+
+        List<Long> mockSourceApplicationIds = List.of(1L, 2L);
+
+        Page<ValueConvertingDto> mockPage = mock(Page.class);
+        when(valueConvertingService.findAllBySourceApplicationIds(
+                any(PageRequest.class), anyBoolean(), eq(mockSourceApplicationIds)
+        )).thenReturn(mockPage);
+
+        ResponseEntity<Page<ValueConvertingDto>> response = controller.getValueConvertings(
+                authentication, 0, 10, "property", Sort.Direction.ASC, false
+        );
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(mockPage, response.getBody());
+    }
+
+    @Test
+    public void shouldReturnHttp200IfGetValueConvertingFound() {
         ValueConvertingDto dto = ValueConvertingDto.builder().build();
         when(valueConvertingService.findById(1L)).thenReturn(Optional.of(dto));
 
@@ -77,15 +105,44 @@ public class ValueConvertingControllerTest {
     }
 
     @Test
-    public void testGetValueConvertingNotFound() {
+    public void shouldThrowExceptionForbiddenIfUserDontHaveAccessToValueConverting() throws NoSuchFieldException, IllegalAccessException {
+        setUserPermissionsConsumerEnabled();
+
+        Jwt jwt = mock(Jwt.class);
+        when(jwt.getClaimAsString("sourceApplicationIds")).thenReturn("2");
+
+        when(authentication.getPrincipal()).thenReturn(jwt);
+
+        ValueConvertingDto valueConvertingDto = mock(ValueConvertingDto.class);
+        when(valueConvertingDto.getFromApplicationId()).thenReturn(1L);
+
+        when(valueConvertingService.findById(1L)).thenReturn(Optional.of(valueConvertingDto));
+
+        ResponseStatusException exception = assertThrows(
+                ResponseStatusException.class,
+                () -> controller.getValueConverting(authentication, 1L)
+        );
+
+        assertEquals(HttpStatus.FORBIDDEN, exception.getStatus());
+        assertEquals("You do not have permission to access or modify data that is related to source application with id=1", exception.getReason());
+    }
+
+
+    @Test
+    public void shouldThrowExceptionWhenGetValueConvertingIsNotFound() {
         when(valueConvertingService.findById(1L)).thenReturn(Optional.empty());
 
-        assertThrows(ResponseStatusException.class, () -> controller.getValueConverting(authentication, 1L));
+        ResponseStatusException exception = assertThrows(
+                ResponseStatusException.class,
+                () -> controller.getValueConverting(authentication, 1L)
+        );
+
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
     }
 
     @SuppressWarnings("unchecked")
     @Test
-    public void testPostValueConvertingWithValidationErrors() {
+    public void shouldThrowExceptionWhenPostingWithValidationErrors() {
         ValueConvertingDto dto = ValueConvertingDto.builder().build();
         Set<ConstraintViolation<ValueConvertingDto>> violations = Collections.singleton(mock(ConstraintViolation.class));
         when(validatorFactory.getValidator().validate(dto)).thenReturn(violations);
@@ -95,7 +152,27 @@ public class ValueConvertingControllerTest {
     }
 
     @Test
-    public void testPostValueConvertingWithNoErrors() {
+    public void shouldThrowExceptionForbiddenIfUserDontHaveAccessToValueConvertingOnPost() throws NoSuchFieldException, IllegalAccessException {
+        setUserPermissionsConsumerEnabled();
+
+        Jwt jwt = mock(Jwt.class);
+        when(jwt.getClaimAsString("sourceApplicationIds")).thenReturn("2");
+        when(authentication.getPrincipal()).thenReturn(jwt);
+
+        ValueConvertingDto valueConvertingDto = mock(ValueConvertingDto.class);
+        when(valueConvertingDto.getFromApplicationId()).thenReturn(1L);
+
+        ResponseStatusException exception = assertThrows(
+                ResponseStatusException.class,
+                () -> controller.postValueConverting(authentication, valueConvertingDto)
+        );
+
+        assertEquals(HttpStatus.FORBIDDEN, exception.getStatus());
+        assertEquals("You do not have permission to access or modify data that is related to source application with id=1", exception.getReason());
+    }
+
+    @Test
+    public void shouldPostValueConvertingWithNoErrors() {
         ValueConvertingDto dto = ValueConvertingDto.builder().build();
         when(validator.validate(dto)).thenReturn(Collections.emptySet());
         when(valueConvertingService.save(dto)).thenReturn(dto);
@@ -105,5 +182,32 @@ public class ValueConvertingControllerTest {
         assertEquals(200, response.getStatusCodeValue());
         assertEquals(dto, response.getBody());
     }
+
+    @Test
+    public void shouldPostValueConvertingWithNoErrorsWhenUserPermissionEnabledAndUserHasAccessToValueConverting() throws NoSuchFieldException, IllegalAccessException {
+        setUserPermissionsConsumerEnabled();
+
+        Jwt jwt = mock(Jwt.class);
+        when(jwt.getClaimAsString("sourceApplicationIds")).thenReturn("2");
+        when(authentication.getPrincipal()).thenReturn(jwt);
+
+        ValueConvertingDto valueConvertingDto = mock(ValueConvertingDto.class);
+        when(valueConvertingDto.getFromApplicationId()).thenReturn(2L);
+
+        when(validator.validate(valueConvertingDto)).thenReturn(Collections.emptySet());
+        when(valueConvertingService.save(valueConvertingDto)).thenReturn(valueConvertingDto);
+
+        ResponseEntity<ValueConvertingDto> response = controller.postValueConverting(authentication, valueConvertingDto);
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(valueConvertingDto, response.getBody());
+    }
+
+    private void setUserPermissionsConsumerEnabled() throws NoSuchFieldException, IllegalAccessException {
+        java.lang.reflect.Field field = ValueConvertingController.class.getDeclaredField("userPermissionsConsumerEnabled");
+        field.setAccessible(true);
+        field.set(controller, true);
+    }
+
 
 }
