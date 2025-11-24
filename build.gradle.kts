e0e3ac63c03c95fb81cc847a1bbc03718a001a94
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2025-11-24 10:18:43 +0100

    Bump latest RC of resource-server, kafka, and flyt-kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,9 +37,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.kafka:spring-kafka")
 
-    implementation("no.novari:flyt-resource-server:6.0.0-rc-25")
-    implementation("no.novari:kafka:5.0.0-rc-19")
-    implementation("no.novari:flyt-kafka:4.0.0-rc-7")
+    implementation("no.novari:flyt-resource-server:6.0.0-rc-26")
+    implementation("no.novari:kafka:5.0.0-rc-20")
+    implementation("no.novari:flyt-kafka:4.0.0-rc-8")
 
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
