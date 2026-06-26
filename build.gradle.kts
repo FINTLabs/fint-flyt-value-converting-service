27056f46f6ea1ac3d324eb44e02fcc8b5505578f
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-06-26 08:56:02 +0200

    FFS-2147: Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id("org.springframework.boot") version "3.5.10"
+    id("org.springframework.boot") version "3.5.15"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.github.ben-manes.versions") version "0.53.0"
-    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
-    kotlin("jvm") version "2.3.10"
-    kotlin("plugin.spring") version "2.3.10"
-    kotlin("plugin.jpa") version "2.3.10"
+    id("com.github.ben-manes.versions") version "0.54.0"
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+    kotlin("jvm") version "2.4.0"
+    kotlin("plugin.spring") version "2.4.0"
+    kotlin("plugin.jpa") version "2.4.0"
 }
 
 group = "no.novari"
@@ -38,9 +38,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-    implementation("no.novari:flyt-web-resource-server:2.0.0")
-    implementation("no.novari:kafka:5.0.0")
-    implementation("no.novari:flyt-kafka:4.0.0")
+    implementation("no.novari:flyt-web-resource-server:3.1.0")
+    implementation("no.novari:flyt-kafka:7.0.0")
 
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
