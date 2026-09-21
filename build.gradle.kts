4c3d14cf26a73bf4b8f1d65b2d53c21ea134ac7c
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-09-21 10:33:03 +0200

    feat: oppdater Flyt-avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,8 +72,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation("no.novari:flyt-web-resource-server:4.0.0")
-    implementation("no.novari:flyt-kafka:7.2.0")
-    implementation("no.novari:flyt-audit-starter:1.1.0")
+    implementation("no.novari:flyt-kafka:7.3.0")
+    implementation("no.novari:flyt-audit-starter:1.2.0")
 
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
